6291845751f98ccd2d4e9baef552db97b1492af7
Nilina Nilaksha <nilina610@gmail.com>
2025-06-28 23:29:36 +0530

    test

--- frontnend/src/pages/vet-services/VetService.jsx
+++ frontnend/src/pages/vet-services/VetService.jsx
@@ -3,7 +3,7 @@ export default function VetService() {
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h2 className="text-xl font-semibold">Vet Services</h2>
-        <p>Under development</p>
+        <p>Under development!</p>
       </div>
     </div>
   );
